9f70ed60af0497169af2f1365662abbd5216eb96
cuuboy <yuzhe.c85@gmail.com>
2018-01-11 15:25:28 +0800

    Implement search service, search data and test cases

--- search_service.rb
+++ search_service.rb
@@ -1,9 +1,44 @@
+require './search_data.rb'
+
+FileNotExistError = StandardError.new("File does not exist.")
+
 class SearchService
-    def initialize(document)
-        @document = document
+    def initialize(document_path)
+        raise FileNotExistError unless File.exists?(document_path)
+        @document_path = document_path
     end
     
     def find!(key_word)
+        data = SearchData.new
+        exam_seq = 0
         
+        file = File.open(@document_path, 'r')
+
+        file.each_char do |c|
+            if c == "\n" 
+                data.move_line
+            else
+                data.move_char
+            end
+
+            if key_word[exam_seq] == c
+                exam_seq += 1
+            else
+                exam_seq = 0
+            end
+
+            if exam_seq >= key_word.size
+                data.found!
+                break 
+            end
+        end
+
+        file.close
+        
+        return data
     end
+
+    private
+
+    attr_reader :document_path
 end
--- search_service_spec.rb
+++ search_service_spec.rb
@@ -1,12 +1,23 @@
 require './search_service.rb'
 
 describe SearchService do
-    it 'Searches provided keyword' do
+    it 'returns search data with found=false if key_word not found' do
+        service = described_class.new('fixtures/projects.json')
+
+        result = service.find!('not_found_test')
+        expect(result.found?).to eq(false)
+    end
+
+    it 'returns search data with found=true if key_word can be found' do
         service = described_class.new('fixtures/projects.json')
 
         result = service.find!('example')
-
-        expect(result).to eq(true)
+        expect(result.found?).to eq(true)
     end
 
+    it 'raises FileNotExistError when file not found the the given path' do
+        expect{
+            described_class.new('fixtures/not_existing_file.json')
+        }.to raise_error(FileNotExistError)
+    end
 end
